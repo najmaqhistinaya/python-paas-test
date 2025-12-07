f2278073e0d0dcf5f5847497b53cb46f2c4935bf
najmaqhistinaya <148700086+najmaqhistinaya@users.noreply.github.com>
2025-12-08 00:11:50 +0700

    Update Flask app message and function name

--- tugas.py
+++ tugas.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Tugas Paas Komputasi Awan!"
+@app.route("/")
+def home():
+    return "Halo! Aplikasi Azure nya berhasil dideploy!"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
